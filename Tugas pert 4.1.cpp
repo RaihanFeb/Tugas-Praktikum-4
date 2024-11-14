24298f06340b6bfa2f2f701ff92989b306c12549
Rai <raihanfebriahdi@gmail.com>
2024-11-14 13:22:09 +0700

    Update Tugas pert 4.1.cpp

--- Tugas pert 4.1.cpp
+++ Tugas pert 4.1.cpp
@@ -1,31 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void fibonacci(int n) {
+int main() {
+    int limit;
+
+    cout << "Masukkan jumlah bilangan Fibonacci yang ingin ditampilkan: ";
+    cin >> limit;
+
     int a = 0, b = 1, nextTerm;
 
-    cout << "Deret Fibonacci: " << a << ", " << b;
+    cout << "Deret Fibonacci: ";
 
-    for (int i = 1; i < n - 1; ++i) {
+    for (int i = 1; i <= limit; ++i) {
+        cout << a << " ";
         nextTerm = a + b;
-        cout << ", " << nextTerm;
         a = b;
         b = nextTerm;
     }
-    cout << endl;
-}
 
-int main() {
-    int n;
-
-    cout << "Masukkan jumlah angka Fibonacci yang diinginkan: ";
-    cin >> n;
-
-    if (n >= 2) {
-        fibonacci(n);
-    } else {
-        cout << "Jumlah angka harus lebih besar atau sama dengan 2." << endl;
-    }
-
-    return 0;
+    return 0;
 }
